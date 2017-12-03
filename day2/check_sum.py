c2dbfd4d67dcfa3ce3b8461cdd20a5d0e462589b
furuhama <furuhama@users.noreply.github.com>
2017-12-04 00:46:52 +0900

    solve day2 part1

--- day2/check_sum.py
+++ day2/check_sum.py
@@ -4,13 +4,36 @@ import sys
 
 
 def shape_raw_text(filename):
+    """
+    get raw text spread sheet
+    convert it to integer list
+    """
     f = open(filename)
     data = f.read()
     f.close()
 
-    print(data)
+    data = data.splitlines()
+    for i in range(len(data)):
+        data[i] = data[i].split('\t')
+
+    # convert every element to integer
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            data[i][j] = int(data[i][j])
+
+    return data
+
+
+def check_difference_min_to_max(l):
+    return max(l) - min(l)
 
 
 if __name__ == '__main__':
     argv = sys.argv
-    shape_raw_text(argv[1])
+    l = shape_raw_text(argv[1])
+
+    my_sum = 0
+    for i in l:
+        my_sum += check_difference_min_to_max(i)
+
+    print(my_sum)
