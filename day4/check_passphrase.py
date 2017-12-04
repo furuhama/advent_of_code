7dba39f0484a35b7464934868534ce4dbb8ace3b
furuhama <furuhama@users.noreply.github.com>
2017-12-04 15:04:42 +0900

    solve day4 part2 to use char vector

--- day4/check_passphrase.py
+++ day4/check_passphrase.py
@@ -25,6 +25,15 @@ def check_valid_phrase_count(word_list):
     return True
 
 
+def get_char_vector(word):
+    lower_cases = [chr(i) for i in range(97, 97 + 26)]
+    result_vector = []
+    for c in lower_cases:
+        result_vector.append(word.count(c))
+
+    return result_vector
+
+
 if __name__ == '__main__':
     w = shape_raw_text('pass_phrases')
 
@@ -37,3 +46,18 @@ if __name__ == '__main__':
     #         count += 1
 
     # print(count)
+
+    #
+    # Part 2
+    #
+    ng_count = 0
+    for e in w:
+        checked_list = []
+        for a in e:
+            if get_char_vector(a) in checked_list:
+                ng_count += 1
+                break
+            else:
+                checked_list.append(get_char_vector(a))
+
+    print(len(w) - ng_count)
