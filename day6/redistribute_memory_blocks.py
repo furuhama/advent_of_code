31ba4fd54ed658029945243e43e127b19bf2dc7d
furuhama <furuhama@users.noreply.github.com>
2017-12-07 00:21:57 +0900

    solve day6 part1

--- day6/redistribute_memory_blocks.py
+++ day6/redistribute_memory_blocks.py
@@ -8,11 +8,79 @@ def shape_raw_input_to_int_list(file):
 
     data = data.split()
 
+    for i in range(len(data)):
+        data[i] = int(data[i])
+
     return data
 
 
-if __name__ == '__main__':
-    n = input()
-    blocks = shape_raw_input_to_int_list(n)
+def which_is_max(memory):
+    """
+    get list & return the place of maximum number
+    """
+    maximum = max(memory)
+    num_list = []
+    for i in range(len(memory)):
+        if memory[i] == maximum:
+            num_list.append(i)
 
-    print(blocks)
+    return num_list
+
+
+def redistribute_blocks(block_list):
+    """
+    main function
+    """
+    memory_size = len(block_list)
+    maximum_place = which_is_max(block_list)[0]
+    maximum_block = block_list[maximum_place]
+
+    base_distribution = maximum_block // memory_size
+    extra_blocks = maximum_block % memory_size
+
+    # redistribution start
+    # redistribute base blocks
+    block_list[maximum_place] = 0
+    for i in range(len(block_list)):
+        block_list[i] += base_distribution
+
+    # redistribute extra blocks
+    for j in range(extra_blocks):
+        target_place = maximum_place + 1 + j
+        if target_place >= memory_size:
+            target_place -= memory_size
+        block_list[target_place] += 1
+
+    return block_list
+
+
+def check_and_update_stock(stock_memory, block_list, f):
+    if block_list in stock_memory:
+        f = True
+        return stock_memory
+    else:
+        stock_memory.append(block_list)
+        f = True
+        return stock_memory
+
+
+if __name__ == '__main__':
+    # n = input()
+    # blocks = shape_raw_input_to_int_list(n)
+    blocks = shape_raw_input_to_int_list('my_input')
+    flag = False
+    stock = []
+    counts = 0
+
+    while not flag:
+        blocks = redistribute_blocks(blocks)
+        if (blocks in stock):
+            flag = True
+        else:
+            stock.append([])
+            for i in range(len(blocks)):
+                stock[counts].append(blocks[i])
+
+        counts += 1
+
+    print(counts)
