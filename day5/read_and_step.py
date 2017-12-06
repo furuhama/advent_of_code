74b5f6260e3691bdc89e46a3019419f9a9d825fb
furuhama <furuhama@users.noreply.github.com>
2017-12-06 15:35:16 +0900

    solve day5 part1

--- day5/read_and_step.py
+++ day5/read_and_step.py
@@ -1,21 +1,28 @@
 """ this is a code for Advent of Code 2017 Day 5 """
 
 
-def read_num_and_get_next_target(place):
+def read_num_and_get_next_target(arg_map, place):
     """
     this function returns next targets place
     place and its number is in list object
     """
+    number = arg_map[place][1]
+    next_place = place + number
 
-    return True
+    # increase the number after move
+    arg_map[place][1] += 1
+
+    return next_place
 
 
-def check_end(place):
+def check_end(arg_map, place):
     """
     check if it reaches an end of input file
     """
-
-    return True  # if it reached the end
+    if place + 1 > len(arg_map):
+        return True
+    else:
+        return False
 
 
 def raw_input_file_to_list(file):
@@ -27,11 +34,20 @@ def raw_input_file_to_list(file):
     for i in range(len(data)):
         temp = []
         temp.append(i + 1)  # .append(int(data[i]))
-        temp.append(data[i])
+        temp.append(int(data[i]))
         data[i] = temp
 
     return data
 
 
 if __name__ == '__main__':
-    target_list = raw_input_file_to_list('my_input')  # right side works well
+    n = input()
+    target_map = raw_input_file_to_list(n)
+
+    place = 0
+    count = 0
+    while not check_end(target_map, place):
+        count += 1
+        place = read_num_and_get_next_target(target_map, place)
+
+    print(count)
