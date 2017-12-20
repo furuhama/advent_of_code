cd9b8ce789d755ae843797ec155d5f0220d8d0e4
furuhama <furuhama@users.noreply.github.com>
2017-12-21 01:10:45 +0900

    get in stuck day10 part1

--- day10/knot_hash.py
+++ day10/knot_hash.py
@@ -3,26 +3,42 @@
 
 def get_input():
     data = input()
-    return [i for i in data]
-
-
-def reverse_and_move_current(input_num, current_position, input_list, skip_size):
-    pass
+    return [int(i) for i in data.split(',')]
 
 
 def reverse_list(input_num, c_position, input_list):
+    length = len(input_list)
     result = [i for i in input_list]
     for i in range(input_num):
-        if i + c_position + 1 > len(input_list) - 1:
-            c_position -= len(input_list)
-        result[i + c_position + 1] = input_list[c_position + input_num - i]
-
+        to_position = get_correct_position(i + c_position + 1, length)
+        from_position = get_correct_position(
+            c_position + input_num - i, length)
+        result[to_position] = input_list[from_position]
     return result
 
 
-if __name__ == '__main__':
-    # order_list = get_input()
-    l = [i for i in range(10)]
+def get_correct_position(position, list_length):
+    return position % list_length
 
-    l2 = reverse_list(3, 0, l)
-    print(l2)
+
+def get_next_c_position(input_num, c_position, list_length, skip_size):
+    return (c_position + input_num + skip_size) % list_length
+
+
+if __name__ == '__main__':
+    order_list = get_input()
+    main_list = [i for i in range(256)]
+    length = 256
+
+    c_position = 0
+    skip_size = 0
+    for input_num in order_list:
+        main_list = reverse_list(input_num, c_position, main_list)
+        c_position = get_next_c_position(
+            input_num, c_position, length, skip_size)
+        skip_size += 1
+
+    print(main_list)
+
+    print("sum of first two elements is: {}".format(
+        main_list[0] * main_list[1]))
